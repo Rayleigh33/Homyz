884847d21f0b0ad208ac903fd388a91332f20fb5
unknown <anshul2002uniyal@gmail.com>
2023-08-09 19:19:11 +0530

    react commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./component/Header/Header";
+import "./App.css";
+import Hero from "./component/Hero/Hero";
+import Companies from "./component/Companies/Companies";
+import Residencies from "./component/Residencies/Residencies";
+import Value from "./component/Value/Value";
+import Contact from "./component/Contact/Contact";
+import GetStarted from "./component/GetStarted/GetStarted";
+import Footer from "./component/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div className="white-gradient" />
+        <Header />
+        <Hero />
+      </div>
+      <Companies />
+      <Residencies />
+      <Value />
+      <Contact />
+      <GetStarted />
+      <Footer />
     </div>
   );
 }
